e690559f1a0595c785b419bdbbbbd5ba9f2da9e5
Austin Payne <austin.payne.01@gmail.com>
2020-10-06 16:58:29 -0600

    add swift syntax highlighting

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ SHELL_NAME=$(basename "$SHELL")
 OS=$(uname -s)
 
 git submodule init
-git submodule update
+git submodule update --depth=1
 
 source install-zsh
 source install-bash
@@ -18,7 +18,7 @@ if [ ! -d ~/.nvm ]; then
     curl -sLo- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 fi
 
-mkdir -p ~/.vim/pack
+mkdir -p ~/.vim/pack/bundle/start
 mkdir -p ~/.ssh
 
 cp .rc ~/.rc
@@ -28,6 +28,7 @@ cp .gitconfig ~/.gitconfig
 cp .vimrc ~/.vimrc
 cp .screenrc ~/.screenrc
 cp -r .vim/pack/plugins/ ~/.vim/pack/plugins
+cp -r .vim/.swift/utils/vim ~/.vim/pack/bundle/start/swift
 cp -r .ssh/ ~/.ssh
 
 # os specific install
