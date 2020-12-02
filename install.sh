463765516121a577a11a12d9af4efc1dd1944c70
Austin Payne <austin.payne.01@gmail.com>
2020-12-02 11:04:51 -0700

    separate vim installation

--- install.sh
+++ install.sh
@@ -3,11 +3,9 @@
 SHELL_NAME=$(basename "$SHELL")
 OS=$(uname -s)
 
-git submodule init
-git submodule update --depth=1
-
 source install-zsh
 source install-bash
+source install-vim
 
 if [ ! -s ~/.git-prompt.sh ]; then
     curl -sLo ~/.git-prompt.sh \
@@ -18,17 +16,13 @@ if [ ! -d ~/.nvm ]; then
     curl -sLo- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 fi
 
-mkdir -p ~/.vim/pack/bundle/start
 mkdir -p ~/.ssh
 
 cp .rc ~/.rc
 cp .bashrc ~/.bashrc
 cp .zshrc ~/.zshrc
 cp .gitconfig ~/.gitconfig
-cp .vimrc ~/.vimrc
 cp .screenrc ~/.screenrc
-cp -r .vim/pack/plugins/ ~/.vim/pack/plugins
-cp -r .vim/.swift/utils/vim ~/.vim/pack/bundle/start/swift
 cp -r .ssh/ ~/.ssh
 
 # os specific install
